e0a42baaaf9e7c102d5333c229ed036c377b09c0
Thomas Clavier <tom@tcweb.org>
2014-06-11 15:11:48 +0200

    first arg is hostname and exit is > 0 if nothing is generated

--- yaml2vhost.py
+++ yaml2vhost.py
@@ -3,8 +3,13 @@ import sys
 sys.path.append('.')
 from yaml2vhost import *
 
+if len(sys.argv) != 1 :
+    host = sys.argv[1]
+
 vhost = vhost.Vhost()
 data = sys.stdin.read()
-host = "prod1"
 apache = vhost.build(data,host)
-print apache
+if len(apache) < 1:
+    sys.exit(1)
+else:
+    print apache
